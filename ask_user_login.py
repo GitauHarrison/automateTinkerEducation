505fe98a050e0a971c445a885103ccb45c1c1939
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-18 05:31:48 +0300

    Allow user to input data 3 times in case of failier to log in

--- ask_user_login.py
+++ ask_user_login.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import datetime
 
 browser = webdriver.Chrome()
 
@@ -19,20 +20,26 @@ def slack_login():
     continue_elem.send_keys(Keys.ENTER)
     sleep(3)
 
-    sign_in_email_elem = browser.find_element_by_name('email')
-    sign_in_email_elem.click()
-    email = input('What is your email address?')
-    sign_in_email_elem.send_keys(email)
+    for repeat in range(3):
+        sign_in_email_elem = browser.find_element_by_name('email')
+        sign_in_email_elem.click()
+        sign_in_email_elem.clear()
+        email = input('What is your email address? ')
+        sign_in_email_elem.send_keys(email)
 
-    sign_in_password_elem = browser.find_element_by_name('password')
-    sign_in_password_elem.click()
-    password = input('What is your password? ')
-    sign_in_password_elem.send_keys(password)
+        sign_in_password_elem = browser.find_element_by_name('password')
+        sign_in_password_elem.click()
+        sign_in_password_elem.clear()
+        password = input('What is your password? ')
+        sign_in_password_elem.send_keys(password)
 
-    sign_in_button_elem = browser.find_element_by_id('signin_btn')
-    sign_in_button_elem.send_keys(Keys.ENTER)
-    sleep(2)
+        sign_in_button_elem = browser.find_element_by_id('signin_btn')
+        sign_in_button_elem.send_keys(Keys.ENTER)
+        sleep(1.5)
+
+        #break
 
     # Post my commute time
     
+
 slack_login()
