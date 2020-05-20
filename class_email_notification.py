b8ab1a1fdd0627b7b00b2e51f2663b0649bd4925
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-20 05:15:20 +0300

    Get parent info from the excel file

---
+++ class_email_notification.py
@@ -0,0 +1,28 @@
+#! python3
+# class_email_notification.py - automatically sends email to parents in the excel file called course_notification.xlsx
+
+# 1. Open the excell file
+# 2. Find parent information
+# 3. Find course information
+# 4. Find student information
+# 5. Link student information to the course information
+# 6. Send email to parents
+
+import openpyxl, smtplib
+from time import sleep
+
+wb = openpyxl.load_workbook('course_notification.xlsx')
+sheet = wb['student info']
+
+parent = {}
+class_info = {}
+students = {}
+
+for r in range(2, sheet.max_row + 1):
+    parent_name = sheet.cell(row = r, column = 8).value
+    parent_email = sheet.cell(row = r, column = 9).value
+    parent[parent_name] = parent_email
+
+    sleep(3)
+
+    
